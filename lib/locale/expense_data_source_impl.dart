5f380fe4c1489eca8947d8c5e525f4916dcbe50c
MarekAniol <marekaniol1@gmail.com>
2023-07-05 13:53:30 +0200

    Implemented HiveService

--- lib/locale/expense_data_source_impl.dart
+++ lib/locale/expense_data_source_impl.dart
@@ -1,28 +1,27 @@
 import 'package:expense_tracker/domain/data/expense_data_source.dart';
 import 'package:expense_tracker/domain/models/expense.dart';
+import 'package:expense_tracker/locale/hive_service.dart';
 import 'package:expense_tracker/locale/models/expense_local_model.dart';
-import 'package:hive/hive.dart';
 
 class ExpenseDataSourceImpl extends ExpenseDataSource {
   ExpenseDataSourceImpl({
-    required this.expenseBox,
-  });
-
-  final Box<ExpenseLocalModel> expenseBox;
+    required HiveService hiveService,
+  }) : _hiveService = hiveService;
+  final HiveService _hiveService;
 
   @override
   Future<Expense> createExpense(Expense expense) async {
     final localModel = expense.toLocalModel();
-    await expenseBox.put(
+    await _hiveService.expenseBox.put(
       localModel.id,
       localModel,
     );
-    final retrivedModel = expenseBox.get(localModel.id) ?? localModel;
+    final retrivedModel = _hiveService.expenseBox.get(localModel.id) ?? localModel;
     return retrivedModel.toDomainModel();
   }
 
   @override
   Future<void> deleteExpense(int id) async {
-    await expenseBox.delete(id);
+    await _hiveService.expenseBox.delete(id);
   }
 }
