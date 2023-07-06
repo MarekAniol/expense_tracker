5089f2c6dfc3f5284854e85aafa0afb6360c0537
MarekAniol <marekaniol1@gmail.com>
2023-07-06 12:33:33 +0200

    Update expense dara source impl tests

--- test/locale/expense_data_source_impl_test.mocks.dart
+++ test/locale/expense_data_source_impl_test.mocks.dart
@@ -5,7 +5,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i3;
 
-import 'package:expense_tracker/locale/hive_service.dart' as _i4;
+import 'package:expense_tracker/locale/data/hive_service.dart' as _i4;
 import 'package:expense_tracker/locale/models/expense_local_model.dart' as _i5;
 import 'package:hive/hive.dart' as _i2;
 import 'package:mockito/mockito.dart' as _i1;
