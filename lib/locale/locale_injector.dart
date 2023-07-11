333330c720ad184afca7e8087528d970f2875534
MarekAniol <marekaniol1@gmail.com>
2023-07-11 13:19:53 +0200

    Updated hive registred in getIt

--- lib/locale/locale_injector.dart
+++ lib/locale/locale_injector.dart
@@ -1,17 +1,18 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:expense_tracker/domain/data/expense_data_source.dart';
 import 'package:expense_tracker/locale/data/hive_local_data.dart';
 import 'package:expense_tracker/locale/expense_data_source_impl.dart';
 import 'package:expense_tracker/locale/hive_local_data_impl.dart';
-import 'package:expense_tracker/locale/models/expense_local_model.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
 extension LocaleInjection on GetIt {
-  void registerLocale() {
-    _registerHive();
+  Future<void> registerLocale() async {
+    await _registerHive();
+    await allReady();
     registerFactory<ExpenseDataSource>(
       () => ExpenseDataSourceImpl(
         hiveLocalData: get<HiveLocalData>(),
@@ -24,21 +25,17 @@ extension LocaleInjection on GetIt {
       ..registerSingleton<HiveInterface>(Hive)
       ..registerSingleton<Directory>(
         await getApplicationDocumentsDirectory(),
-      )
-      ..registerSingletonAsync<Box<ExpenseLocalModel>>(() async {
-        await allReady();
-        final box = await Hive.openBox<ExpenseLocalModel>('expenseBox');
-        return box;
-      })
-      ..registerSingletonAsync<HiveLocalData>(() async {
-        await allReady();
+      );
+
+    registerSingletonAsync<HiveLocalData>(
+      () async {
         final hiveLocalData = HiveLocalDataImpl(
           hive: get<HiveInterface>(),
           documentsDirectory: get<Directory>(),
-          expenseBox: get<Box<ExpenseLocalModel>>(),
         );
         await hiveLocalData.initHive();
         return hiveLocalData;
-      });
+      },
+    );
   }
 }
