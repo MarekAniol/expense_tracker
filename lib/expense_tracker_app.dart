4a2fd651dad9dd2918a1bb75d5376047ca89ed48
MarekAniol <marekaniol1@gmail.com>
2023-07-24 11:18:57 +0200

    Added custom theme and colors

--- lib/expense_tracker_app.dart
+++ lib/expense_tracker_app.dart
@@ -1,22 +1,28 @@
 import 'package:expense_tracker/presentaion/bloc/create_new_expense/create_new_expense_bloc.dart';
 import 'package:expense_tracker/presentaion/injector.dart';
 import 'package:expense_tracker/presentaion/route/expense_tracker_route.dart';
+import 'package:expense_tracker/presentaion/theme/app_theme.dart';
+import 'package:expense_tracker/presentaion/theme/color_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExpenseTrackerApp extends StatelessWidget {
   ExpenseTrackerApp({
     required this.appRouter,
+    required this.appTheme,
     super.key,
   });
   final navigatorKey = GlobalKey<NavigatorState>();
   final ExpenseTrackerAppRouter appRouter;
+  final AppTheme appTheme;
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => injector<CreateNewExpenseBloc>(),
       child: MaterialApp(
+        theme: appTheme.theme(LightPalette()),
+        darkTheme: appTheme.theme(DarkPalette()),
         navigatorKey: navigatorKey,
         onGenerateRoute: appRouter.route,
         builder: (context, child) => _BaseScreen(
