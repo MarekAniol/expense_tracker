e266b1c2f50c1cc6a06d10da2212ee9c7a6833ab
MarekAniol <marekaniol1@gmail.com>
2023-07-27 11:55:02 +0200

    Added orientation UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,23 @@ import 'package:expense_tracker/presentaion/injector.dart' as injector;
 import 'package:expense_tracker/presentaion/route/expense_tracker_route.dart';
 import 'package:expense_tracker/presentaion/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight
+    ],
+  ).then(
+    (_) => _runAppAsync(),
+  );
+}
+
+Future<void> _runAppAsync() async {
   await injector.init();
   runApp(
     ExpenseTrackerApp(
--- lib/presentaion/screens/home_screen/home_screen.dart
+++ lib/presentaion/screens/home_screen/home_screen.dart
@@ -1,10 +1,10 @@
+import 'package:expense_tracker/presentaion/extensions/context_extensions.dart';
 import 'package:expense_tracker/presentaion/screens/home_screen/bloc/chart_bloc/chart_bloc.dart';
 import 'package:expense_tracker/presentaion/screens/home_screen/bloc/home_screen_bloc.dart';
 import 'package:expense_tracker/presentaion/screens/home_screen/widgets/expenses_list.dart';
 import 'package:expense_tracker/presentaion/widgets/chart/chart.dart';
 import 'package:expense_tracker/presentaion/widgets/custom_app_bar.dart';
 import 'package:expense_tracker/presentaion/widgets/snack_bar_message.dart';
-import 'package:expense_tracker/presentaion/widgets/validate_dialog_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,36 +17,47 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          BlocListener<HomeScreenBloc, HomeScreenState>(
-            listenWhen: (previous, current) =>
-                (previous.expenses.isEmpty && previous.expenses != current.expenses) ||
-                (current.expenses.isNotEmpty && previous.expenses != current.expenses),
-            listener: (context, state) {
-              context.read<ChartBloc>().add(
-                    ChartEvent.expenseBucketsCreated(
-                      expenses: state.expenses,
-                    ),
-                  );
+      body: BlocListener<HomeScreenBloc, HomeScreenState>(
+        listenWhen: (previous, current) => previous.expenses != current.expenses,
+        listener: (context, state) {
+          context.read<ChartBloc>().add(
+                ChartEvent.expenseBucketsCreated(
+                  expenses: state.expenses,
+                ),
+              );
+        },
+        child: SnackBarMessage(
+          child: BlocBuilder<ChartBloc, ChartState>(
+            buildWhen: (previous, current) => previous.expenseBuckets != current.expenseBuckets,
+            builder: (context, state) {
+              return const _ContentPageOrientation();
             },
-            child: BlocBuilder<ChartBloc, ChartState>(
-              buildWhen: (previous, current) => current.expenseBuckets.isNotEmpty,
-              builder: (context, state) {
-                return const Chart();
-              },
-            ),
           ),
-          const Expanded(
-            child: SnackBarMessage(
-              child: ValidateDialogMessage(
-                child: ExpensesList(),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
+  }
+}
+
+class _ContentPageOrientation extends StatelessWidget {
+  const _ContentPageOrientation();
+
+  @override
+  Widget build(BuildContext context) {
+    return context.orientation == Orientation.portrait
+        ? const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Chart(),
+              Expanded(child: ExpensesList()),
+            ],
+          )
+        : const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Expanded(child: Chart()),
+              Expanded(child: ExpensesList()),
+            ],
+          );
   }
 }
